53aeed8864404442a357097c7c67b8ad9e0ea403
Isaac Wilkinson-Longden <isaacwilkinsonlongden@gmail.com>
2025-07-12 13:31:37 +0100

    added player class

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Deck:
         suits = ["♥", "♦", "♣", "♠"]
         ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
                   "J", "Q", "K", "A"]
+        
         for suit in suits:
             for rank in ranks:
                 self.deck.append(Card(rank, suit))
@@ -33,6 +34,40 @@ class Deck:
         return self.deck.pop()
     
 
-d = Deck()
-d.shuffle()
-print(d.deal_card())
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def get_hand_value(self):
+        total = 0
+        ace_count = 0
+
+        for card in self.hand:
+            if card.rank.isdigit():
+                total += int(card.rank)
+            elif card.rank in ["J", "Q", "K"]:
+                total += 10
+            elif card.rank == "A":
+                total += 11
+                ace_count += 1
+
+        while total > 21 and ace_count > 0:
+            total -= 10
+            ace_count -= 1
+
+        return total
+    
+    def show_hand(self):
+        return ' '.join(str(card) for card in self.hand)
+    
+
+p = Player("Test")
+p.add_card(Card("A", "♠"))
+p.add_card(Card("K", "♦"))
+print(p.get_hand_value())  # Should print 21
+
+
